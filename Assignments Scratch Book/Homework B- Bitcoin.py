dec9f0b73edc544b6fdabc3266322564b675f5e0
Monica Dabas <monica.dabas@gmail.com>
2017-02-12 17:52:42 -0500

    Q1 solved- year 2140

--- Assignments Scratch Book/Homework B- Bitcoin.py
+++ Assignments Scratch Book/Homework B- Bitcoin.py
@@ -7,9 +7,19 @@ def Q1():
     n = 0 # number of times reward is halved
     bitcoins = 21*(10**6) # total bitcoins to be mined
     reward = 50
+
+    # in every cycle 210000 blocks are mined which means atleast 210000 satoshis should be added in each cycle
+    # reward halving cycle runs till bitcoins added are not less than 210000 satoshis
+    # cycle must run only till number of bitcoins mined are less than equal to the maximum bitcoins
+
     while not(total >= bitcoins):
-        total += reward*(1/(2**(n))) * 21 * (10**4)
-        n += 1
+        bitcoins_added = round(reward*(1/(2**(n))) * 21 * (10**4), 8)
+        print(bitcoins_added)
+        if bitcoins_added >= 21 * (10**(-4)):
+            total += bitcoins_added
+            n += 1
+        else:
+            break
         print("total: {}, n: {}".format(total,n))
 
     total_minutes = n * 21 * 10**5 # minutes required to mine all blocks
@@ -18,6 +28,6 @@ def Q1():
     past_time = datetime.datetime(2009, month = 1, day = 3, hour = 18, minute = 15, second = 5)
 
     t = past_time + datetime.timedelta(minutes = total_minutes)
-    print(t)
+    print(t, t.year)
 
 Q1()
