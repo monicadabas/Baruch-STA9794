89892ccbad84a5cfb55b8075f5ff22811c5b35c4
Monica Dabas <monica.dabas@gmail.com>
2017-02-22 19:16:10 -0500

    Sequential program complete

--- Assignments Scratch Book/Project1Scrub.py
+++ Assignments Scratch Book/Project1Scrub.py
@@ -2,6 +2,11 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from math import log, e
 import numpy as np
+import logging
+from operator import itemgetter
+
+
+logging.basicConfig(filename ="Project1Scrub_log.log", level= logging.DEBUG, filemode='w')
 
 
 def date_parse(s):
@@ -13,10 +18,38 @@ def date_parse(s):
         return "Error"
 
 
-def isvalid(row):
-    if len(row) == 3 and isinstance(row["Price"],float) and isinstance(row["Units"],int) and row["Timestamp"] != "Error":
-        return True
-    return False
+class IsValidResult:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+
+def isvalid(row,t):
+    t0 = date_parse("00010101:00:00:00.000000")
+    if len(row) == 3 and isinstance(row[1],float) and row[1]>0 and isinstance(row[2],np.int64) and row[2]>0 and row[0] != "Error":
+        if t < row[0]:
+            if not((row[0]-t).total_seconds() > 3) or t == t0:
+                return IsValidResult(True, row[0])
+            else:
+                logging.info("Timestamps is more than 3 seconds than earlier timestamp")
+                return IsValidResult(False,t)
+
+        elif t == row[0]:
+            logging.info("Duplicate timestamp")
+            return IsValidResult(False,t)
+        else:
+            if -3 < (t-row[0]).total_seconds() < 3:
+                return IsValidResult(True, t)
+            else:
+                logging.info("Timestamp is more than 3 seconds earlier")
+                return IsValidResult(False, t)
+    else:
+        if row[0] == "Error":
+            logging.info("Incorrect timestamp format")
+            return IsValidResult(False,max(t,row[0]))
+        else:
+            logging.info("Initial condition of datatypes and range not met")
+            return IsValidResult(False,t)
 
 
 df = pd.read_csv("data.txt", header=None)
@@ -26,60 +59,68 @@ df.Units = df.Units.astype(int)
 
 
 def segregate(df):
-    signal = []
-    noise = []
-    df.Timestamp = df.Timestamp.apply(lambda x: date_parse(x))
+    signal = list()
+    noise = list()
+    t = date_parse("00010101:00:00:00.000000")
     for i in range(len(df)):
-        if not isvalid(df.loc[i]):
+        logging.info("Row number: {}".format(i))
+        row = list()
+        row.append(date_parse(df.loc[i].Timestamp))
+        row.append(df.loc[i].Price)
+        row.append(df.loc[i].Units)
+
+        result = isvalid(row, t)
+
+        if not result.first:
+            logging.info("Unit and Datatype: {}, {}".format(row[2],type(row[2])))
+            logging.info("Length: {}".format(len(row) == 3))
+            logging.info("Is Price float: {}".format(isinstance(row[1],float)))
+            logging.info("Is price greater than 0: {}".format(row[1]>0))
+            logging.info("Is Unit int: {}".format(isinstance(row[2],np.int64)))
+            logging.info("Is Unit greater than 0:{}".format(row[2]>0))
+            logging.info("Is Timestamp correct: {}".format(row[0] != "Error"))
             noise.append(i)
 
-    dt = date_parse("00000000:00:00:02.000000")
+        else:
+            t = result.second
+            signal.append([date_parse(df.loc[i].Timestamp),df.loc[i].Price])
 
-    print(noise)
-
-
-segregate(df)
-# i = 1
-# lt = df.Timestamp[i-1]
-# while i <= 100000:
-#     if abs(df.Timestamp[i-1] - df.Timestamp[i]) < dt:
-#         if df.Timestamp[i-1] > df.Timestamp[i]:
-#             df.Timestamp[i], df.Timestamp[i-1] = df.Timestamp[i-1], df.Timestamp[i]
-#         signal.append(df.Index[i-1])
-#         signal.append(df.Index[i])
-#
-#     else:
-#         if df.Timestamp[i-1] > df.Timestamp[i]:
-#             signal.append(df.Index[i-1])
+    print(len(noise))
+    print("Signal Length: {}".format(len(signal)))
+    return signal
 
 
+signal_Prices = segregate(df)
 
-#df.sort_values(by=['Timestamp'], ascending=True)
-#df.Price = pd.to_numeric(df.Price, errors='coerce')
-#df["Previous_Price"] = df.Price.shift(-1)
+signal_Prices = sorted(signal_Prices, key=itemgetter(0))
+print("Sorted signal length: {}".format(len(signal_Prices)))
+dups = []
+for i in range(1,len(signal_Prices)):
+    if signal_Prices[i][0] == signal_Prices[i-1][0]:
+        dups.append(signal_Prices[i][0])
 
-#df["Returns"] = df.Price / df.Previous_Price
+print("Length of dups: {}".format(len(dups)))
 
-#df["Log_Returns"] = np.log(df.Returns)
-#df["Returns"] = np.log(df.Price) - np.log(df.Price.shift(-1))
-#df["Log_Returns"] = np.log(1-df.Returns)
+print("Signal Prices printing")
 
-print(df.tail())
-bins = np.linspace(-10,10)
-# X = [i for i in range(100000)]
-# plt.scatter(X,df.Returns)
-# plt.ylim((-8, 8))
-# plt.show()
-X = range(-10, 10, 1)
-#ydf = pd.crosstab(X, df.Returns)
-# Y = pd.cut(df.Returns,bins)
-# print(Y)
-#
-# plt.scatter(X,Y)
-# plt.show()
-#mu = df.Returns.mean()
-#mu = np.mean(df.Returns)
-#sigma = df.Returns.std()
+i = 0
+while i < 1:
+    print("time in signal:{}".format(signal_Prices[i][0]))
+    print("time in dups: {}".format(dups[i]))
+    i += 1
 
-#print(mu, sigma)
-#print(np.min(df.Returns))
+for i in signal_Prices:
+    if i[0] in dups:
+        del i
+
+print("Sorted and no duplicates signal length: {}".format(len(signal_Prices)))
+returns = []
+for i in range(1,len(signal_Prices)):
+    ret = log(signal_Prices[i][1]/ signal_Prices[i-1][1], e)
+    returns.append(ret*100)
+
+print(len(returns), max(returns), min(returns))
+
+plt.hist(returns, bins=np.arange(-150, 150))
+plt.show()
+
