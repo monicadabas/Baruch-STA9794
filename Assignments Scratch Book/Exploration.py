29d5893a985097444c1dc15dca385892936009b2
Monica Dabas <monica.dabas@gmail.com>
2017-02-27 02:04:10 -0500

    Price Explore

--- Assignments Scratch Book/Exploration.py
+++ Assignments Scratch Book/Exploration.py
@@ -1,11 +1,7 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-#from math import log, e
 import numpy as np
 
-x = 'o'
-x = float(x)
-print(isinstance(x,float))
 
 
 def date_parse(s):
@@ -21,28 +17,25 @@ df.columns = ["Timestamp", "Price", "Units"]
 df.Price = df.Price.astype(float)
 df.Units = df.Units.astype(int)
 
-print(min(df.Price), min(df.Units))
-#df.Timestamp = df.Timestamp.apply(lambda x: date_parse(x))
-#t0 = date_parse(df.loc[0].Timestamp)
-Y = []
-ts_min = date_parse("00010101:00:00:00.000000")
-y = 0
-#print(type(ts_min), type(t0))
-for i in range(len(df)):
-    t = date_parse(df.loc[i].Timestamp)
-    # print(t)
-    # print(ts_min)
-    # print(abs(t-ts_min).total_seconds())
-    if not isinstance(t, str) and (abs(t-ts_min).total_seconds() <= 3 or t>ts_min):
-        if t>ts_min:
-            ts_min = t
-        continue
-        # y += 1
-        # Y.append(y)
-        # if t > ts_min:
-        #     ts_min = t
-    else:
-        #y -= 1
-        Y.append(i)
-print(len(Y))
-print(Y)
+#print(min(df.Price), min(df.Units))
+
+# Y = []
+# ts_min = date_parse("00010101:00:00:00.000000")
+# y = 0
+#
+# for i in range(len(df)):
+#     t = date_parse(df.loc[i].Timestamp)
+#
+#     if not isinstance(t, str) and (abs(t-ts_min).total_seconds() <= 3 or t>ts_min):
+#         if t>ts_min:
+#             ts_min = t
+#         continue
+#
+#     else:
+#
+#         Y.append(i)
+# print(len(Y))
+# print(Y)
+df.Price = df.Price.apply(lambda x: x/100)
+plt.hist(df.Price, bins=np.arange(-10, 30))
+plt.show()
