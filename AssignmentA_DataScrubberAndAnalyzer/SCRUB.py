fb01c78f232982360d00ab2ab13b55dc6044421a
Monica Dabas <monica.dabas@gmail.com>
2017-02-27 16:36:16 -0500

    declared a function to identify noise

--- AssignmentA_DataScrubberAndAnalyzer/SCRUB.py
+++ AssignmentA_DataScrubberAndAnalyzer/SCRUB.py
@@ -1,5 +1,6 @@
 from mpi4py import MPI
 import numpy as np
+import pandas as pd
 import sys
 import os
 
@@ -9,7 +10,7 @@ class Block:
         self.start = start
         self.end = end
 
-# change this function to get noise and signal files
+# return number of ticks or rows in each block (each node processes one block)
 def get_line_count(fh,block):
     buffer_size = block.end + 1 - block.start
     buffer = np.empty(buffer_size, dtype=str)
@@ -25,14 +26,42 @@ def get_line_count(fh,block):
     return count
 
 
+#identify noise and signal and write them in separate files
+def identify_noise(file, block, count):
+    print("File format is : {}".format(type(file)))
+    #file.Seek(block.start)
+    #with open(file, 'r') as file:
+    file.Seek(block.start)
+    while count > 0:
+        buffer_size = min(10, count)
+        chunker = file.Readlines(chunksize=buffer_size)
+        count -= buffer_size
+        print("Length of chunker: {}, Count: {}".format(len(chunker), count))
+
+        #lines = [line for line in file][:count]
+
+
+
+
+
+
 def adjust_blocks(fh,block,rank, nprocs):
     buffer_size = 100
     buffer = np.empty(buffer_size, dtype=str)
+    error = fh.Set_errhandler(MPI.ERRORS_RETURN)
 
     # adjust the block start
     if not(rank == 0):
         fh.Seek(block.start)
+        if error is not None:
+            print("Could not seek file")
+            MPI.Finalize()
+
+
         fh.Read([buffer, MPI.CHAR])
+        if error is not None:
+            print("Could not read file")
+            MPI.Finalize()
         #print("Process: {}, Start buffer: {}".format(rank,buffer))
 
         for i in range(buffer_size):
@@ -43,7 +72,15 @@ def adjust_blocks(fh,block,rank, nprocs):
     # adjust the block end
     if not(rank == nprocs-1):
         fh.Seek(block.end)
+        if error is not None:
+            print("Call to seek file failed")
+            MPI.Finalize()
+
+
         fh.Read([buffer, MPI.CHAR])
+        if error is not None:
+            print("Call to read file failed")
+            MPI.Finalize()
         #print("Process: {}, End buffer: {}".format(rank,buffer))
 
         for i in range(buffer_size):
@@ -52,9 +89,11 @@ def adjust_blocks(fh,block,rank, nprocs):
                 break
 
     print("Process: {}, Adjusted block: [{}, {}]".format(rank,block.start,block.end))
-
+    #return Block(block.start, block.end)
     count = get_line_count(fh,block)
     print("Process: {}, Line Count: {}".format(rank,count))
+
+    return block, count
 
 
 def main(argv):
@@ -63,8 +102,11 @@ def main(argv):
     rank = comm.Get_rank()
     nprocs = comm.Get_size()
     fh = MPI.File.Open(comm, argv)
+    error = fh.Set_errhandler(MPI.ERRORS_RETURN)
     file_size = fh.Get_size()
     block_size = int(file_size/nprocs)
+    #print("Error: {}".format(error))
+
 
     # divide file into blocks for each process (same length)
     block_start = rank * block_size
@@ -78,7 +120,9 @@ def main(argv):
 
     print("Process {}: Block: [{}, {}]".format(rank,block.start,block.end))
 
-    adjust_blocks(fh,block,rank, nprocs)
+    block, count = adjust_blocks(fh,block,rank, nprocs)
+
+    identify_noise(fh,block,count)
 
 
 if __name__ == "__main__":
