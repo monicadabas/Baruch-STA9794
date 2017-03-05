dbe6acd504c7b98bc9abdaa3daeb3b08aceeac66
Monica Dabas <monica.dabas@gmail.com>
2017-03-05 16:42:52 -0500

    modfied to test result with scrub

--- Assignments Scratch Book/Project1Scrub.py
+++ Assignments Scratch Book/Project1Scrub.py
@@ -56,6 +56,11 @@ df = pd.read_csv("data.txt", header=None)
 df.columns = ["Timestamp", "Price", "Units"]
 df.Price = df.Price.astype(float)
 df.Units = df.Units.astype(int)
+# for i in range(len(df)):
+#     df.loc[i].Timestamp = date_parse(df.loc[i].Timestamp)
+df.Timestamp.apply(date_parse)
+
+print(df.describe())
 
 
 def segregate(df):
@@ -83,21 +88,22 @@ def segregate(df):
 
         else:
             t = result.second
-            signal.append([date_parse(df.loc[i].Timestamp),df.loc[i].Price])
+            signal.append([date_parse(df.loc[i].Timestamp),df.loc[i].Price,i])
 
-    print(len(noise))
+    print("Lenght of Noise: {}".format(len(noise)))
     print("Signal Length: {}".format(len(signal)))
-    return signal
+    return signal,noise
 
 
-signal_Prices = segregate(df)
+
+signal_Prices,noise = segregate(df)
 
 signal_Prices = sorted(signal_Prices, key=itemgetter(0))
 print("Sorted signal length: {}".format(len(signal_Prices)))
 dups = []
 for i in range(1,len(signal_Prices)):
     if signal_Prices[i][0] == signal_Prices[i-1][0]:
-        dups.append(signal_Prices[i][0])
+        dups.append(signal_Prices[i][2])
 
 print("Length of dups: {}".format(len(dups)))
 
@@ -106,14 +112,16 @@ print("Signal Prices printing")
 i = 0
 while i < 1:
     print("time in signal:{}".format(signal_Prices[i][0]))
-    print("time in dups: {}".format(dups[i]))
+    print("time in dups:  {}".format(dups[i]))
     i += 1
 
 for i in signal_Prices:
-    if i[0] in dups:
+    if i[2] in dups:
+        noise.append(i[2])
         del i
 
-print("Sorted and no duplicates signal length: {}".format(len(signal_Prices)))
+l = len(signal_Prices)
+print("Sorted and no duplicates signal length: {}".format(l))
 returns = []
 for i in range(1,len(signal_Prices)):
     ret = log(signal_Prices[i][1]/ signal_Prices[i-1][1], e)
@@ -123,4 +131,39 @@ print(len(returns), max(returns), min(returns))
 
 plt.hist(returns, bins=np.arange(-150, 150))
 plt.show()
+
+price_total = 0
+for i in signal_Prices:
+    price_total += i[1]
+
+print("Stats of signal Prices")
+#total = reduce(lambda a,b: a+b,signal_Prices[:][1])
+print(price_total/l)
+
+print("Length of Noise: {}".format(len(noise)))
+
+with open("noise_scratch.txt","w") as noise_file:
+    for index in noise:
+        noise_file.write(str(index)+"\n")
+
+#               Price         Units
+# count  1.000000e+05  100000.00000
+# mean   1.297625e+03  375162.20130
+# std    2.066845e+04  216440.21781
+# min   -1.612682e+06 -714867.00000
+# 25%    1.128760e+03  187366.25000
+# 50%    1.273880e+03  375241.00000
+# 75%    1.444060e+03  562024.50000
+# max    1.413027e+06  749998.00000
+# Length of Noise: 157
+# Signal Length: 99843
+# Sorted signal length: 99843
+# Length of dups: 86
+# Signal Prices printing
+# time in signal:2014-08-04 10:00:13.002033
+# time in dups:  2014-08-04 10:00:14.643038
+# Sorted and no duplicates signal length: 99843
+# (99842, 697.5809272370919, -733.812676039931)
+# Stats of signal Prices
+# 1495.71196168
 
